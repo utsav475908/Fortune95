0e668a5b6dd2cbc1c7167a77106c9aab2778f9a7
Kumar Utsav <Kumar.Utsav@honeywell.com>
2016-04-11 14:12:47 +0530

    Initial Commit

---
+++ SKYBellCOAPManager/SKYBellCOAPManager.h
@@ -0,0 +1,19 @@
+//
+//  SKYBellCOAPManager.h
+//  SKYBellCOAPManager
+//
+//  Created by Kumar Utsav on 11/04/16.
+//  Copyright © 2016 Kumar Utsav. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKYBellCOAPManager.
+FOUNDATION_EXPORT double SKYBellCOAPManagerVersionNumber;
+
+//! Project version string for SKYBellCOAPManager.
+FOUNDATION_EXPORT const unsigned char SKYBellCOAPManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKYBellCOAPManager/PublicHeader.h>
+
+
